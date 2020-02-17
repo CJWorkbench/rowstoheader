f0e9d233d3148cc4570280f99df5e5a099e07d05
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-17 14:45:00 +0200

    Add minimum cjwmodule version to Pipfile and setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     url="https://github.com/CJWorkbench/rowstoheader",
     packages=[""],
     py_modules=["rowstoheader"],
-    install_requires=["pandas==0.25.0", "cjwmodule==*"],
+    install_requires=["pandas==0.25.0", "cjwmodule>=1.4.0"],
 )

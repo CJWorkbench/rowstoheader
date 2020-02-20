f7b35dfb3973d80017097d910f34ba18c0ef2ade
Adam Hooper <adam@adamhooper.com>
2020-02-20 11:08:33 -0500

    Use gen_unique_clean_colnames_and_warn()
    Should fix errors on production. There may be a few changes to existing
    workflows (because renumbering works differently -- "better" -- now);
    hopefully the warnings on the module will help users adjust.

--- rowstoheader.py
+++ rowstoheader.py
@@ -3,22 +3,25 @@ from typing import Any, Dict, Tuple, List
 import pandas as pd
 from pandas import IntervalIndex
 from cjwmodule import i18n
+from cjwmodule.util.colnames import gen_unique_clean_colnames_and_warn
 
 
-commas = re.compile('\\s*,\\s*')
-numbers = re.compile('(?P<first>[1-9]\d*)(?:-(?P<last>[1-9]\d*))?')
+commas = re.compile("\\s*,\\s*")
+numbers = re.compile("(?P<first>[1-9]\d*)(?:-(?P<last>[1-9]\d*))?")
+
 
 class RangeFormatError(ValueError):
     def __init__(self, value):
         self.value = value
-        
+
     @property
     def i18n_message(self) -> i18n.I18nMessage:
         return i18n.trans(
             "badParam.rows.invalidRange",
             'Rows must look like "1-2", "5" or "1-2, 5"; got "{value}"',
-            {"value": self.value}
+            {"value": self.value},
         )
+
 
 def parse_interval(s: str) -> Tuple[int, int]:
     """
@@ -39,8 +42,8 @@ def parse_interval(s: str) -> Tuple[int, int]:
     if not match:
         raise RangeFormatError(s)
 
-    first = int(match.group('first'))
-    last = int(match.group('last') or first)
+    first = int(match.group("first"))
+    last = int(match.group("last") or first)
     return (first - 1, last - 1)
 
 
@@ -50,56 +53,14 @@ class Form:
         self.delete_above = delete_above
 
     @staticmethod
-    def parse(d: Dict[str, Any]) -> 'Form':
-        rows = d.get('rows', '')
-        tuples = [parse_interval(s)
-                  for s in commas.split(rows.strip()) if s.strip()]
-        index = IntervalIndex.from_tuples(tuples, closed='both')
+    def parse(d: Dict[str, Any]) -> "Form":
+        rows = d.get("rows", "")
+        tuples = [parse_interval(s) for s in commas.split(rows.strip()) if s.strip()]
+        index = IntervalIndex.from_tuples(tuples, closed="both")
 
-        delete_above = d.get('deleteabove', False)
+        delete_above = d.get("deleteabove", False)
 
         return Form(index, delete_above)
-
-
-def _make_names_unique(names: List[str]) -> List[str]:
-    """Append to each name if necessary, to make a list without duplicates."""
-    counts = {}
-
-    def unique_name(ideal_name: str) -> str:
-        """
-        Generate a guaranteed-unique column name, using `counts` as state.
-
-        No-op if ideal_name is empty. (We'll rename empty columns later.)
-
-        Strategy for making 'A' unique:
-        * If 'A' has never been seen before, return it.
-        * If 'A' has been seen before, try 'A_1' or 'A_2' (where 1 and 2 are
-          the number of times 'A' has been seen).
-        * If there is a conflict on 'A_1', recurse.
-        """
-        if ideal_name not in counts or not ideal_name:
-            counts[ideal_name] = 1
-            return ideal_name
-
-        count = counts[ideal_name]
-        counts[ideal_name] += 1
-        backup_name = f'{ideal_name}_{count}'
-        return unique_name(backup_name)
-
-    def rename_empty(ideal_name: str, index: int) -> str:
-        """
-        Rename an empty column to "Column 1" ... or "Column 1_1" if the latter
-        is taken.
-        """
-        if not ideal_name:
-            ideal_name = 'Column %d' % (index + 1)
-            return unique_name(ideal_name)
-        else:
-            return ideal_name
-
-    names_including_empty = [unique_name(name) for name in names]
-    return [rename_empty(name, i)
-            for i, name in enumerate(names_including_empty)]
 
 
 def process(table: pd.DataFrame, form: Form) -> pd.DataFrame:
@@ -112,9 +73,9 @@ def process(table: pd.DataFrame, form: Form) -> pd.DataFrame:
         # the default.
         return table
     header_rows_str = header_rows.astype(str)
-    header_rows_str[header_rows.isna()] = ''
-    names = list(header_rows_str.agg(lambda x: ' – '.join(s for s in x if s)))
-    unique_names = _make_names_unique(names)
+    header_rows_str[header_rows.isna()] = ""
+    names = list(header_rows_str.agg(lambda x: " – ".join(s for s in x if s)))
+    unique_names, warnings = gen_unique_clean_colnames_and_warn(names)
 
     if form.delete_above:
         max_header_index = max(t[1] for t in form.index.to_tuples())
@@ -125,9 +86,12 @@ def process(table: pd.DataFrame, form: Form) -> pd.DataFrame:
     table.columns = unique_names
     for column in unique_names:
         series = table[column]
-        if hasattr(series, 'cat'):
+        if hasattr(series, "cat"):
             series.cat.remove_unused_categories(inplace=True)
-    return table
+    if warnings:
+        return (table, warnings)
+    else:
+        return table
 
 
 def render(table, params):
--- test_rowstoheader.py
+++ test_rowstoheader.py
@@ -3,110 +3,149 @@ import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from rowstoheader import render
-from cjwmodule.testing.i18n import i18n_message
+from cjwmodule.testing.i18n import i18n_message, cjwmodule_i18n_message
 
 
 class TestRowsToHeader(unittest.TestCase):
     def test_first_row(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3],
-            'B': ['foo', 'bar', None],
-            'C': [None, None, None],
-        }), {'rows': '1', 'deleteabove': True})
-        assert_frame_equal(out, pd.DataFrame({
-            '1': [2, 3],
-            'foo': ['bar', None],
-            'Column 3': [None, None],
-        }))
+        out = render(
+            pd.DataFrame({"A": [1, 2, 3], "B": ["foo", "bar", None]}),
+            {"rows": "1", "deleteabove": True},
+        )
+        assert_frame_equal(out, pd.DataFrame({"1": [2, 3], "foo": ["bar", None]}))
 
     def test_multiple_rows(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3, 4],
-            'B': ['foo', None, '', 'moo'],
-            'C': [None, None, None, None],
-        }), {'rows': '1-3', 'deleteabove': True})
-        assert_frame_equal(out, pd.DataFrame({
-            '1 – 2 – 3': [4],
-            'foo': ['moo'],
-            'Column 3': [None]
-        }))
+        out = render(
+            pd.DataFrame(
+                {
+                    "A": [1, 2, 3, 4],
+                    "B": ["foo", None, "", "moo"],
+                    "C": [None, None, "Column 3", None],
+                }
+            ),
+            {"rows": "1-3", "deleteabove": True},
+        )
+        assert_frame_equal(
+            out, pd.DataFrame({"1 – 2 – 3": [4], "foo": ["moo"], "Column 3": [None]})
+        )
 
     def test_keep_middle_rows(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3, 4],
-            'B': ['foo', None, '', 'moo'],
-            'C': [None, None, None, None],
-        }), {'rows': '1,3', 'deleteabove': False})
-        assert_frame_equal(out, pd.DataFrame({
-            '1 – 3': [2, 4],
-            'foo': [None, 'moo'],
-            'Column 3': [None, None]
-        }))
+        out = render(
+            pd.DataFrame({"A": [1, 2, 3, 4], "B": ["foo", None, "", "moo"]}),
+            {"rows": "1,3", "deleteabove": False},
+        )
+        assert_frame_equal(out, pd.DataFrame({"1 – 3": [2, 4], "foo": [None, "moo"]}))
 
     def test_drop_middle_rows(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3, 4],
-            'B': ['foo', None, '', 'moo'],
-            'C': [None, None, None, None],
-        }), {'rows': '1,3', 'deleteabove': True})
-        assert_frame_equal(out, pd.DataFrame({
-            '1 – 3': [4],
-            'foo': ['moo'],
-            'Column 3': [None],
-        }))
+        out = render(
+            pd.DataFrame(
+                {
+                    "A": [1, 2, 3, 4],
+                    "B": ["foo", None, "", "moo"],
+                    "C": [None, None, "Column 3", None],
+                }
+            ),
+            {"rows": "1,3", "deleteabove": True},
+        )
+        assert_frame_equal(
+            out, pd.DataFrame({"1 – 3": [4], "foo": ["moo"], "Column 3": [None]})
+        )
 
     def test_avoid_duplicate_columns(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3, 4],
-            'B': [1, 2, 3, 4],
-            'C': [1, 2, 3, 4],
-            'D': ['1', '2_1', '3', '4'],
-        }), {'rows': '1-2', 'deleteabove': False})
-        assert_frame_equal(out, pd.DataFrame({
-            '1 – 2': [3, 4],
-            '1 – 2_1': [3, 4],
-            '1 – 2_2': [3, 4],
-            '1 – 2_1_1': ['3', '4'],
-        }))
+        out = render(
+            pd.DataFrame(
+                {
+                    "A": [1, 2, 3, 4],
+                    "B": [1, 2, 3, 4],
+                    "C": [1, 2, 3, 4],
+                    "D": ["1", "2_1", "3", "4"],
+                }
+            ),
+            {"rows": "1-2", "deleteabove": False},
+        )
+        assert_frame_equal(
+            out[0],
+            pd.DataFrame(
+                {
+                    "1 – 2": [3, 4],
+                    "1 – 3": [3, 4],
+                    "1 – 4": [3, 4],
+                    "1 – 2_1": ["3", "4"],
+                }
+            ),
+        )
+        self.assertEqual(
+            out[1],
+            [
+                cjwmodule_i18n_message(
+                    "util.colnames.warnings.numbered",
+                    {"n_columns": 2, "first_colname": "1 – 3"},
+                )
+            ],
+        )
 
     def test_avoid_empty_columns(self):
-        out = render(pd.DataFrame({
-            'A': ['', 'x'],
-            'B': [np.nan, 'y'],
-            'C': [None, 'z'],
-            'D': ['Column 1', 'a'],  # curve-ball: prevent rename
-        }), {'rows': '1', 'deleteabove': False})
-        assert_frame_equal(out, pd.DataFrame({
-            'Column 1_1': ['x'],
-            'Column 2': ['y'],
-            'Column 3': ['z'],
-            'Column 1': ['a'],
-        }))
+        out = render(
+            pd.DataFrame(
+                {
+                    "A": ["", "x"],
+                    "B": [np.nan, "y"],
+                    "C": [None, "z"],
+                    "D": ["Column 1", "a"],  # curve-ball: prevent rename
+                }
+            ),
+            {"rows": "1", "deleteabove": False},
+        )
+        assert_frame_equal(
+            out[0],
+            pd.DataFrame(
+                {
+                    "Column 4": ["x"],
+                    "Column 2": ["y"],
+                    "Column 3": ["z"],
+                    "Column 1": ["a"],
+                }
+            ),
+        )
+        self.assertEqual(
+            out[1],
+            [
+                cjwmodule_i18n_message(
+                    "util.colnames.warnings.default",
+                    {"n_columns": 3, "first_colname": "Column 4"},
+                ),
+                cjwmodule_i18n_message(
+                    "util.colnames.warnings.numbered",
+                    {"n_columns": 1, "first_colname": "Column 4"},
+                ),
+            ],
+        )
 
     def test_no_rows_no_op(self):
-        out = render(pd.DataFrame({
-            'A': [1, 2, 3],
-            'B': ['foo', 'bar', None],
-            'C': [None, None, None],
-        }), {'rows': '', 'deleteabove': True})
-        assert_frame_equal(out, pd.DataFrame({
-            'A': [1, 2, 3],
-            'B': ['foo', 'bar', None],
-            'C': [None, None, None],
-        }))
+        out = render(
+            pd.DataFrame(
+                {"A": [1, 2, 3], "B": ["foo", "bar", None], "C": [None, None, None]}
+            ),
+            {"rows": "", "deleteabove": True},
+        )
+        assert_frame_equal(
+            out,
+            pd.DataFrame(
+                {"A": [1, 2, 3], "B": ["foo", "bar", None], "C": [None, None, None]}
+            ),
+        )
 
     def test_remove_unused_categories(self):
         result = render(
-            pd.DataFrame({'A': ['a', 'b', 'c']}, dtype='category'),
-            {'rows': '2', 'deleteabove': True}
+            pd.DataFrame({"A": ["a", "b", "c"]}, dtype="category"),
+            {"rows": "2", "deleteabove": True},
         )
-        assert_frame_equal(result,
-                           pd.DataFrame({'b': ['c']}, dtype='category'))
-
+        assert_frame_equal(result, pd.DataFrame({"b": ["c"]}, dtype="category"))
 
     def test_zero_gives_error(self):
-        out = render(pd.DataFrame({'A': ['a', 'b', 'c']}, dtype='category'), {'rows': '0-1'})
+        out = render(
+            pd.DataFrame({"A": ["a", "b", "c"]}, dtype="category"), {"rows": "0-1"}
+        )
         self.assertEqual(
-            out,
-            i18n_message("badParam.rows.invalidRange", {"value":"0-1"})
+            out, i18n_message("badParam.rows.invalidRange", {"value": "0-1"})
         )
